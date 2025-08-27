1ebaeed2df511acf0c6b033827f70817f25ce13c
yaso28 <26593159+yaso28@users.noreply.github.com>
2025-08-27 19:49:06 +0900

    modified SDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.yaso202508appproxy.intunetestapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.yaso202508appproxy.intunetestapp"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
